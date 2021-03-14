bc8a6226dc09a5b5616aa97526fe1130e4902715
mono <mono@mono0x.net>
2021-03-15 01:38:24 +0900

    webpack@5

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,17 +1,17 @@
-import path from 'path';
-import webpack from 'webpack';
-import CleanWebpackPlugin from 'clean-webpack-plugin';
-import CopyWebpackPlugin from 'copy-webpack-plugin';
-import ChromeExtensionReloader from 'webpack-chrome-extension-reloader';
+import path from 'path'
+import webpack from 'webpack'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+import ChromeExtensionReloader from 'webpack-chrome-extension-reloader'
 
 export default {
   entry: {
     background: path.join(__dirname, 'src', 'scripts', 'background.js'),
-    content: path.join(__dirname, 'src', 'scripts', 'content.js'),
+    content: path.join(__dirname, 'src', 'scripts', 'content.js')
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'scripts/[name].js',
+    filename: 'scripts/[name].js'
   },
   target: 'web',
   devtool: 'cheap-module-source-map',
@@ -19,37 +19,46 @@ export default {
     rules: [
       {
         test: /\.js$/,
-        use: { loader: 'babel-loader' },
-      },
-    ],
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' }
+      }
+    ]
   },
   plugins: [
-    new CleanWebpackPlugin([
-      path.join(__dirname, 'dist'),
-    ]),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        path.join(__dirname, 'dist')
+      ]
+    }),
     new webpack.EnvironmentPlugin([
-      'NODE_ENV',
+      'NODE_ENV'
     ]),
-    new CopyWebpackPlugin([
-      {
-        from: path.join(__dirname, 'src', 'manifest.json'),
-        to: path.join(__dirname, 'dist'),
-        transform: (content, path) => {
-          var manifest = JSON.parse(content.toString());
-          manifest.version = process.env.npm_package_version;
-          return Buffer.from(JSON.stringify(manifest));
-        },
-      },
-    ]),
-    ...(process.env.NODE_ENV === 'development' ? [
-      new ChromeExtensionReloader({
-        entries: {
-          background: 'background',
-          contentScript: [
-            'content',
-          ],
-        },
-      }),
-    ] : []),
-  ],
-};
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, 'src', 'manifest.json'),
+          to: path.join(__dirname, 'dist'),
+          transform: (content, path) => {
+            return Buffer.from(JSON.stringify({
+              ...JSON.parse(content.toString()),
+              version: process.env.npm_package_version
+            }))
+          }
+        }
+      ]
+    }),
+    ...(process.env.NODE_ENV === 'development'
+      ? [
+          new ChromeExtensionReloader({
+            entries: {
+              background: 'background',
+              contentScript: [
+                'content'
+              ]
+            }
+          })
+        ]
+      : []
+    )
+  ]
+}
